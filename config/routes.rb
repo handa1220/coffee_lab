19611fcb5dca809eae56d47d17c67aa43d7e8560
handa1220 <108671890+handa1220@users.noreply.github.com>
2023-01-19 14:15:11 +0000

    [create]コントローラー、ルートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :public do
+    resources :reviews, only: [:index, :show, :new, :create, :edit, :update, :search, :destroy]
+    resources :customers, only: [:show, :edit, :update]
+  end
+
+  namespace :admin do
+    root to: "homes/top"
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :reviews, only: [:index, :show, :edit, :update, :destroy]
+  end
+
+
 end
