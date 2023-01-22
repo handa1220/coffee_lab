18face89f6eefe08f2a59b243b2eb8a7e2b82584
handa1220 <108671890+handa1220@users.noreply.github.com>
2023-01-22 12:25:20 +0000

    [update]model修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,9 @@
 class Review < ApplicationRecord
+
+  enum degree_of_roasting: {light_roast: 0, medium_roast: 1, dark_roast: 2 }
+
+  belongs_to :customer
+  has_many :comments
+  has_many :likes
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :public do
+    root to: "homes#top"
     resources :reviews, only: [:index, :show, :new, :create, :edit, :update, :search, :destroy]
     resources :customers, only: [:show, :edit, :update]
   end
